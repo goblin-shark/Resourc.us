c4e533045e5eafdd11c0e423f96da17670a769bd
Sean Yalda <sensei@seans-mbp.home>
2021-03-27 14:04:33 -0700

    installed bcrypt and setup user model and presave hash

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const Schema = mongoose.Schema;
 
-const teamSchema = new Schema({
-    image: { type: String },
-    name: { type: String, required: true, unique: true },
-    category: { type: String, required: true },
-    description: { type: String }
+const userSchema = new Schema({
+    email: { type: String, required: [true, "can't be blank"], unique: true, index: true },
+	hash: { type: String, required: [true, "can't be blank"], min: [8, 'Not enough characters'] },
+	salt: String,
+	accessGranted: { type: Boolean, required: true }
 }, { timestamps: true });
 
-var Team = mongoose.model('Team', teamSchema);
+const saltRounds = 10;
+userSchema.pre('save', function(next) {
+	const user = this;
+	if(!user.isModified('hash')) return next();
+	bcrypt.genSalt(saltRounds, function(err, salt) {
+		if (err) return next(err);
+		bcrypt.hash(user.hash, saltRounds, function(err, hash) {
+			if (err) return next(err);
+			user.hash = hash;
+			next()
+		});
+	});
+});
 
-module.exports = { Team };
+var User = mongoose.model('User', userSchema);
+
+module.exports = { User };
