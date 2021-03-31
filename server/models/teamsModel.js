442f9f8655407d64735bef59ad95b3a2038a82ce
Eric Wilding <ewilding8@gmail.com>
2021-03-30 22:21:07 -0500

    Add votes to resource model and adds upvote route. Also add user/resource count to teams schema

--- server/models/teamsModel.js
+++ server/models/teamsModel.js
@@ -5,7 +5,9 @@ const teamSchema = new Schema({
     image: { type: String },
     name: { type: String, required: true, unique: true },
     category: { type: String, required: true },
-    description: { type: String }
+    description: { type: String },
+    userCount: { type: Number },
+    resourceCount: { type: Number }
 }, { timestamps: true });
 
 var Team = mongoose.model('Team', teamSchema);
