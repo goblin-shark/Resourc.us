bf353c5b34a20ada8a9817f77cf502201a6f8b0b
jenpoole <contact.vandepool@gmail.com>
2021-04-01 01:13:18 -0400

    #4 link to teamPageURL

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react"
-
+import React, {useState, useEffect} from "react";
+import { Link, Route } from 'react-router-dom';
 
 function Teams() {
   const [_teams, setTeams] = useState([]);
@@ -15,15 +15,23 @@ function Teams() {
       console.log('GET FAILED', err);
     })
   })
+  
   return (
     <div className='container'>
       <h1>Teams Page</h1>
       {_teams.map( team => 
-        <div key={team.name}>
+        <div className="teamCard" key={team.name}>
           <h1>{team.name}</h1>
           <h1>{team.image}</h1>
           <h1>{team.description}</h1>
           <h1>{team.category}</h1>
+
+          {/* link to Team Detail Page */}
+          <Link className="btn btn-primary" to={"/teams/" + team.name.toLowerCase().trim().replace(" ", "-")}>View</Link>
+          <Route path={"/teams/" + team.name.toLowerCase().trim().replace(" ", "-")} exact>{team.name}</Route>
+          
+          {/* link to join Team */}
+          <Link className="btn btn-success" to="/#">Join</Link>
         </div>
       )}
     </div>
@@ -31,3 +39,5 @@ function Teams() {
 }
 
 export default Teams;
+
+// const teamPageUrl = team.name
