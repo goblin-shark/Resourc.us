d767be7453e7f43aef31bc4b12a83d3a3c270c30
jenpoole <contact.vandepool@gmail.com>
2021-04-01 00:46:15 -0400

    remove duplicate code

--- src/App.js
+++ src/App.js
@@ -35,3 +35,37 @@ function App() {
   );
 }
 export default App;
+
+
+// Other Routes
+{/* <div className="App"> */}
+  // <nav className="navbar">
+  //   <ul className="navbar-nav">
+  //     <li>
+  //       <Link to="/">Home</Link>
+  //     </li>
+  //     <li>
+  //       <Link to="/signup">Sign Up</Link>
+  //     </li>
+  //     <li>
+  //       <Link to="/login">Log In</Link>
+  //     </li>
+  //     <li>
+  //       <Link to="/teams">Teams</Link>
+  //     </li>
+  //   </ul>
+  // </nav>
+  {/* define the routes of the application */}
+  // <Switch>
+  //   <Route exact path="/" component={HomePage}></Route>
+  //   <Route exact path="/signup" component={SignupPage}></Route>
+  //   <Route exact path="/login" component={LoginPage}></Route>
+  //   <Route exact path="/teams" component={AllTeamsPage}></Route>
+  //   {/* 
+  //   <Route path="/teams/create" component={}></Route>
+  //   <Route path="/teams/edit/:id" component={}></Route>
+  //   <Route path="/teams/:id" component={}></Route> 
+  //   <Route path="/users/:id" component={}></Route> 
+  //   */}
+  // </Switch>
+// </div>
