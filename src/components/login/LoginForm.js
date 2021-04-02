45228c445e8ba26b6e3b3df448cf036a687fe6f8
Sean Yalda <hayawan@seans-mbp.home>
2021-04-02 16:47:29 -0700

    signup and login components are now wired up to the model and controller

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 
 export const LoginForm = () => {
+  const [values, setValues] = useState({
+		email: '',
+		password: '',
+	});
   const { register, handleSubmit } = useForm()
   const history = useHistory()
 
   // 'data' is an object where the keys are the names of the form fields, 
   // and the values are the form input values
   const onSubmit = handleSubmit((data) => {
-    alert(JSON.stringify(data))
+    fetch('/user/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values)
+    })
+      .then(resp => console.log(resp))
+      .then(data => {
+        // Enter something that stores or handles cookies or JWT
+        history.push("/");
+      })
+      .catch(err => console.log('Auth Form won\'t fetch, error:', err));
+    // alert(JSON.stringify(data))
     // redirect to Homepage after successful login
-    history.push('/')
+    // history.push('/')
   })
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const id = e.target.id;
+
+    const valuesCopy = values;
+    valuesCopy[id] = value;
+    setValues(valuesCopy);
+    console.log(values);
+  }
 
   return (
     <form onSubmit={onSubmit}>
       <div className="form-group">
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" ref={register}></input>
+        <input type="email" name="email" id="email" ref={register} onChange={handleChange} />
       </div>
 
       <div className="form-group">
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" ref={register}></input>
+        <input type="password" name="password" id="password" ref={register} onChange={handleChange} />
       </div>
 
       <div className="form-group">
