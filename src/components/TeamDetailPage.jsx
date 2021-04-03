551b186f6ce50247d60f711a9a76e20adcea7332
jenpoole <contact.vandepool@gmail.com>
2021-04-03 14:47:58 -0400

    get resources

--- src/components/TeamDetailPage.jsx
+++ src/components/TeamDetailPage.jsx
@@ -4,38 +4,37 @@ import React, {useState, useEffect} from "react";
 function TeamDetailPage ({ match }) {
   // get the team ID from the URL params (destructure props.match.params)
   const { params: { id } } = match;
-  // console.log( 'params:', params)
   // console.log( 'props:', props)
-  console.log( 'id:', id)
+  // console.log( 'params:', params)
+  // console.log( 'id:', id)
 
   // set Team info in state
   const [team, setTeam] = useState([]);
+
+  // set Team Resources in state
+  const [teamResources, setTeamResources] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/teams/list")
     .then(response => response.json())
     .then(data => {
       const team = data.filter(t => t._id === id)
-      console.log('teams data:', data)
-      console.log('individual team data:', team)
+      // console.log('teams data:', data)
+      // console.log('individual team data:', team)
       setTeam(team)
+    })
+    .then(() => {
+      fetch("http://localhost:3000/resource/listAll")
+      .then(response => response.json())
+      .then(data => {
+        console.log("resources: ", data)
+        setTeamResources(data)
+      })
     })
     .catch(err => {
       console.log('GET FAILED', err);
     })
   }, [])
-
-  // fetch teams list and filter by team name (from URL params)
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/teams/list")
-  //   .then(response => response.json())
-  //   .then(teams => {
-  //     const team = teams.filter((t) => t.name.toLowerCase() === teamName.replace(/[-]/g, ' '))
-  //     setTeam(team)
-  //   }).catch(err => {
-  //     console.log('Failed to get team', err);
-  //   })
-  // }, [])
 
   return (
     <div>
@@ -45,6 +44,15 @@ function TeamDetailPage ({ match }) {
         <p>Category: {t.category}</p>
         <p>Description: {t.description}</p>
       </div>)}</div>
+
+      <div>
+        <h1>Resources</h1>
+        <div>{teamResources.map(resource => <div key={resource._id}>
+          <div>{resource.title}</div>
+          <div>{resource.link}</div>
+          <div>{resource.votes}</div>
+        </div>)}</div>
+      </div>
     </div>
   );
 }
