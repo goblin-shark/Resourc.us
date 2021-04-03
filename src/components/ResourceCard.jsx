e20d83d3f3be32f257816284ba078a23e0c91890
Ethan Yeh <ehwyeh@ucdavis.edu>
2021-04-04 02:33:05 +0800

    works but endless loop

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -5,30 +5,51 @@ import React, { useState, useEffect } from "react";
 // Put request to upvote and downvote
 
 function ResourceCard() {
-  const [_resource, setResource] = useState({});
+  const [_resource, setResource] = useState([]);
+  const [_count, setCount] = useState(0)
   // const [_upvote, setUpvote] = useState({});
+  const _payload={"teamId": "606275b0cb1a340bdc7d8121"}
   useEffect(() => {
-    fetch("http://localhost:3000/resource/listAll")
+    console.log(_payload)
+    fetch("http://localhost:3000/resource/list", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(_payload),
+    })
       .then((response) => {
-        return response.json(); //Parses to JSON
+        return response.json();
       })
       .then((data) => {
-        setResource(data[0]);
-        // console.log(data); ENDLESS RUNNING BUG!?
+        // console.log(data);
+        setResource(data)
+        console.log('_resource:',_resource)
       })
       .catch((err) => {
-        console.log("GET FAILED", err);
+        console.log("Post Fail", err);
       });
-  }, [_resource]);
+  }, [_count]);
 
+//get resource id
+//get current resource vote
+//update state
   function handleUpvote(event) {
+    setCount(2);
     event.preventDefault();
+    const parent = document.getElementById("ethan");
+    const teamid = parent.getAttribute("value");
+    const link = parent.getAttribute("link");
+    const votes = Number(parent.getAttribute("votes"));
+    console.log('HERE',teamid);
     const payload = {
-      "link" : _resource.link,
-      "teamid": _resource.teamid,
-      "votes": _resource.votes,
+      "link" : link,
+      "teamId": teamid,
+      "votes": votes,
       "upvote": true
     }
+    console.log('payload:', payload)
     // POST the payload to database
     fetch("http://localhost:3000/resource/upvote", {
       method: "POST",
@@ -42,21 +63,34 @@ function ResourceCard() {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
-        setResource(data)
+        console.log('data sent back:' ,data);
+        const newResource = _resource;
+        for (let i = 0; i < newResource.length; i++) {
+          if (newResource[i].link === data.link) {
+            newResource[i] = data;
+          }
+        }
+        setResource(newResource)
       })
       .catch((err) => {
         console.log("Post Fail", err);
       });
   }
   function handleDownvote(event) {
+    setCount(1);
     event.preventDefault();
+    const parent = document.getElementById("ethan");
+    const teamid = parent.getAttribute("value");
+    const link = parent.getAttribute("link");
+    const votes = Number(parent.getAttribute("votes"));
+    console.log('HERE',teamid);
     const payload = {
-      "link" : _resource.link,
-      "teamid": _resource.teamid,
-      "votes": _resource.votes,
+      "link" : link,
+      "teamId": teamid,
+      "votes": votes,
       "upvote": false
     }
+    console.log('payload:', payload)
     // POST the payload to database
     fetch("http://localhost:3000/resource/upvote", {
       method: "POST",
@@ -70,8 +104,14 @@ function ResourceCard() {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
-        setResource(data)
+        console.log('data sent back:' ,data);
+        const newResource = _resource;
+        for (let i = 0; i < newResource.length; i++) {
+          if (newResource[i].link === data.link) {
+            newResource[i] = data;
+          }
+        }
+        setResource(newResource)
       })
       .catch((err) => {
         console.log("Post Fail", err);
@@ -79,13 +119,28 @@ function ResourceCard() {
   }
   return (
     <div>
-      <h1>Resource Card</h1>
+      {/* <h1>Resource Card</h1>
       <div>
         <h1>{_resource.link}</h1>
         <h1>{_resource.votes}</h1>
         <button onClick={handleUpvote}>Upvote</button>
         <button onClick={handleDownvote}>Downvote</button>
-      </div>
+      </div> */}
+      {_resource.map((resource) => (
+  <div
+    id="ethan"
+    className="teamCard"
+    value={resource.teamId}
+    key={resource._id}
+    link={resource.link}
+    votes={resource.votes}
+  >
+    <h1>{resource.link}</h1>
+    <h1>{resource.votes}</h1>
+    <button onClick={handleUpvote}>Upvote</button>
+    <button onClick={handleDownvote}>Downvote</button>
+  </div>
+))}
     </div>
   );
 }
