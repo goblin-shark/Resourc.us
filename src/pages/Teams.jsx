2f26e551b7b0b97da99a3924daf8ed520c282747
Ethan Yeh <ehwyeh@ucdavis.edu>
2021-04-03 15:51:01 +0800

    Create Resource Working

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,25 +1,27 @@
-import React, {useState, useEffect} from "react";
-import { Link, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, Route } from "react-router-dom";
 
 function Teams() {
   const [_teams, setTeams] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/teams/list").then(response => {
-      
-      return response.json(); //Parses to JSON
-    }).then(data => {
-      setTeams(data);
-      // console.log(data); ENDLESS RUNNING BUG!?
-    }).catch(err => {
-      console.log('GET FAILED', err);
-    })
-  })
-  
+    fetch("http://localhost:3000/teams/list")
+      .then((response) => {
+        return response.json(); //Parses to JSON
+      })
+      .then((data) => {
+        setTeams(data);
+        // console.log(data); ENDLESS RUNNING BUG!?
+      })
+      .catch((err) => {
+        console.log("GET FAILED", err);
+      });
+  }, []);
+
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Teams Page</h1>
-      {_teams.map( team => 
+      {_teams.map((team) => (
         <div className="teamCard" key={team.name}>
           <h1>{team.name}</h1>
           <h1>{team.image}</h1>
@@ -27,14 +29,21 @@ function Teams() {
           <h1>{team.category}</h1>
 
           {/* link to Team Detail Page */}
-          <Link className="btn btn-primary" to={"/teams/" + team.name.toLowerCase().trim().replace(/\s/g, "-")}>View</Link>
-          
+          <Link
+            className="btn btn-primary"
+            to={"/teams/" + team.name.toLowerCase().trim().replace(/\s/g, "-")}
+          >
+            View
+          </Link>
+
           {/* link to join Team */}
-          <Link className="btn btn-success" to="/#">Join</Link>
+          <Link className="btn btn-success" to="/#">
+            Join
+          </Link>
         </div>
-      )}
+      ))}
     </div>
-  )
+  );
 }
 
 export default Teams;
