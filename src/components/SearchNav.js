86e1f483f952e968151d47f2ab7f1180f30835eb
Julia Collins <collins.acumed@gmail.com>
2021-04-14 14:05:11 -0700

    noResult page created and rendering, search deferring to tab with data

--- src/components/SearchNav.js
+++ src/components/SearchNav.js
@@ -1,35 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useLayoutEffect } from 'react'
 import { Tabs, Tab} from 'react-bootstrap';
 import Home from '../pages/Home';
-import Teams from '../pages/Teams'
-import ResourceCard from '../components/ResourceCard'
+import Teams from '../pages/Teams';
+import ResourceCard from '../components/ResourceCard';
+import NoResourcesFound from '../components/NoResourcesFound';
 
 const SearchNav = (props) => {
 
   const teamSearch = props.location.state.search.team;
-  const resourceSearch = props.location.state.search.tags;
+  const resourceSearch = props.location.state.search.resourceSearch;
   const allInitialResources = props.location.state.resources;
 
-//RECEIVING allInitialResources Array from initial load
   
-  const [_key, setKey] = useState('Teams');
+  const [_key, setKey] = useState('');
+  const [_defaultKey, setDefaultKey] = useState('Teams');
 
-    // useEffect(() => {
-    //   if(!teamSearch && resourceSearch) setKey('Resources')
-    //   if(teamSearch && !resourceSearch) setKey('Teams')
-    // })
+
+   useLayoutEffect(() => {
+    setDefaultKey(resourceSearch.length && ! teamSearch.length ? 'Resources' : 'Teams')
+    console.log(_defaultKey)
+   }, [props])
 
       return (
         <div>
-            <Tabs defaultActiveKey={!teamSearch.length ? 'Resources' : 'Teams'}
-                  id="controlled-tab-example">
+            <Tabs defaultActiveKey={_defaultKey}
+                  activeKey = {_key.length && _key !== _defaultKey ? _key : _defaultKey}
+                  id="controlled-tab-example"
+                  onSelect={(k) => setKey(k)}
+                  >
                 <Tab eventKey="Teams" title="Teams">
                 <br></br>
-                    {teamSearch.length ? <Teams teams = {teamSearch} /> : <h3>No search results for 'Teams' found</h3>}
+                    {teamSearch.length ? <Teams teams = {teamSearch} /> : <NoResourcesFound type={'teams'}/>}
                 </Tab>
                 <Tab eventKey="Resources" title="Resources"> 
                 <br></br>
-                {resourceSearch.length ? <ResourceCard resources= {resourceSearch} ></ResourceCard> : <h3>No search results for 'Resources' found</h3>}
+                {resourceSearch.length ? <ResourceCard resources= {resourceSearch} ></ResourceCard> : <NoResourcesFound type={'resources'}/>}
                 </Tab>
             </Tabs>
         </div>
