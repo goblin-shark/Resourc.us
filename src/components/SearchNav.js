4c98a48dc25165d97cdd7498f1c391850f95c88b
Eric Wilding <ewilding8@gmail.com>
2021-04-15 21:04:42 -0500

    Merged working

---
+++ src/components/SearchNav.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState, useLayoutEffect } from 'react'
+import { Tabs, Tab } from 'react-bootstrap';
+import Home from '../pages/Home';
+import Teams from '../pages/Teams';
+import ResourceCard from '../components/ResourceCard';
+import NoResourcesFound from '../components/NoResourcesFound';
+const SearchNav = (props) => {
+  const teamSearch = props.location.state.search.team;
+  const resourceSearch = props.location.state.search.resourceSearch;
+
+  const allInitialResources = props.location.state.resources;
+  console.log(allInitialResources[0].tags)
+
+  const [_key, setKey] = useState('');
+  const [_defaultKey, setDefaultKey] = useState('Teams');
+
+  useLayoutEffect(() => {
+    setDefaultKey(resourceSearch.length && !teamSearch.length ? 'Resources' : 'Teams')
+  }, [])
+
+  return (
+    <div>
+      <Tabs defaultActiveKey={resourceSearch.length && !teamSearch.length ? 'Resources' : 'Teams'}
+        activeKey={_key.length && _key !== _defaultKey ? _key : _defaultKey}
+        id="controlled-tab-example"
+        onSelect={(k) => setKey(k)}
+      >
+        <Tab eventKey="Teams" title="Teams">
+          <br></br>
+          {teamSearch.length ? <Teams teams={teamSearch} /> : <NoResourcesFound type={'Teams'} />}
+        </Tab>
+        <Tab eventKey="Resources" title="Resources">
+          <br></br>
+          {resourceSearch.length ? <ResourceCard resources={resourceSearch} /> : <NoResourcesFound type={'Resources'} />}
+        </Tab>
+      </Tabs>
+    </div>
+  );
+}
+export default SearchNav;
