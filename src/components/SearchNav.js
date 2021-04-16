a9044d8a530cebdedae86110ed86b7c6b61b1e30
Julia Collins <collins.acumed@gmail.com>
2021-04-15 18:55:09 -0700

    polished searchNav component

--- src/components/SearchNav.js
+++ src/components/SearchNav.js
@@ -9,36 +9,53 @@ const SearchNav = (props) => {
 
   const teamSearch = props.location.state.search.team;
   const resourceSearch = props.location.state.search.resourceSearch;
-  const allInitialResources = props.location.state.resources;
 
+  const allInitialResources = props.location.state.resources;
   
   const [_key, setKey] = useState('');
-  const [_defaultKey, setDefaultKey] = useState('Teams');
-
-
+  const defaultKey = 'Teams';
+    
    useLayoutEffect(() => {
-    setDefaultKey(resourceSearch.length && ! teamSearch.length ? 'Resources' : 'Teams')
-    console.log(_defaultKey)
+    setKey(resourceSearch.length && ! teamSearch.length ? 'Resources' : 'Teams')
    }, [props])
 
       return (
         <div>
-            <Tabs defaultActiveKey={_defaultKey}
-                  activeKey = {_key.length && _key !== _defaultKey ? _key : _defaultKey}
+            <Tabs defaultActiveKey={resourceSearch.length && ! teamSearch.length ? 'Resources' : 'Teams'}
+                  activeKey = {_key.length && _key !== defaultKey ? _key : defaultKey}
                   id="controlled-tab-example"
                   onSelect={(k) => setKey(k)}
                   >
                 <Tab eventKey="Teams" title="Teams">
                 <br></br>
-                    {teamSearch.length ? <Teams teams = {teamSearch} /> : <NoResourcesFound type={'teams'}/>}
+                    {teamSearch.length ? <Teams teams = {teamSearch} /> : <NoResourcesFound type={'Teams'}/>}
                 </Tab>
                 <Tab eventKey="Resources" title="Resources"> 
                 <br></br>
-                {resourceSearch.length ? <ResourceCard resources= {resourceSearch} ></ResourceCard> : <NoResourcesFound type={'resources'}/>}
+                {resourceSearch.length ? <ResourceCard resources = {resourceSearch} /> : <NoResourcesFound type={'Resources'}/>}
                 </Tab>
             </Tabs>
         </div>
       );
 }
 
-export default SearchNav
+export default SearchNav;
+
+
+// function Equals(resourceTag, userQuery){
+//   resourceTag = resourceTag.toLowerCase()
+//   userQuery = userQuery.toLowerCase()
+
+//   if(resourceTag == userQuery) return true;
+//   return false
+// }
+
+
+// let resourceArray = [];
+// if(resultTags) {
+//   for(let resources of resultTags) {
+//     if(Equals(resources.tags, request.query.query)){
+//       resourceArray.push(resources)
+//     }
+//   }
+// }
